0b2f0498a52095ec442693a0b130d01b9fbba3a8
Swapnil Singh <swappp7singh@gmail.com>
2017-02-06 23:25:05 +0530

    made the cross icon clear the search artists box and also designed the search filed to look awesome.

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  searchStr: string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  searchMusic() {
+    console.log(this.searchStr);
+  }
+
+  clearStr(): void {
+    this.searchStr = '';
   }
 
 }
